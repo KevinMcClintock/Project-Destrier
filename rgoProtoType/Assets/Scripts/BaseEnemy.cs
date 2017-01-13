d0b4d9e67d7a2dd27dec4f8f052c10beb460f636
KevinMcClintock <JY06YN@europe.intranet>
2017-01-13 15:02:43 +0100

    First Commit
    Added the Unity Project.

---
+++ rgoProtoType/Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[System.Serializable]
+public class BaseEnemy  {
+
+    public string name;
+
+    public enum Type
+    {
+            GRASS,
+            FIRE,
+            WATER,
+            LIGHTNING,
+            ICE
+
+    }
+
+
+    public enum Rarity
+    {
+        COMMON,
+        UNCOMMON,
+        RARE,
+        VERYRARE
+
+    }
+
+    public Type EnemyType;
+    public Rarity rarity;
+
+    public float baseHP;
+    public float currentHP;
+    public float baseMP;
+    public float currentMP;
+    public float baseAttack;
+    public float baseDef;
+    public float currentAttack;
+    public float currentDef;
+
+
+}
---
+++ rgoProtoType/Assets/Scripts/BaseHero.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class BaseHero  {
+
+    public string   name;
+    public float    baseHP;
+    public float    currentHP;
+    public float    baseMP;
+    public float    currentMP;
+    public int      stamina;
+    public int      dexterity;
+    public int      intellect;
+    public int      agility;
+
+}
